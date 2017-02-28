ca206f46f0d03f75187c528047700c621ea560b8
Asep Santana <santanaasep948@yahoo.co.id>
2017-02-28 19:20:12 +0700

    Add files via upload

---
+++ Minggu ke-2/mahasiswa.java
@@ -0,0 +1,8 @@
+/*prgram mahasiswa*/
+public class mahasiswa {
+  public static void main(String argv []){
+      System.out.println("Nama	: Asep Santana");
+	  System.out.println("NIM	: 10112603");
+	  System.out.println("Alamat	: Rancamanyar");
+	  }
+	}
