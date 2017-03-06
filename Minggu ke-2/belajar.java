6926dcb4a18724d6daa26ba926fc3f2e1da6ca52
Asep Santana <santanaasep948@yahoo.co.id>
2017-03-06 16:55:22 +0700

    Add files via upload

---
+++ Minggu ke-2/belajar.java
@@ -0,0 +1,5 @@
+public class belajar {
+  public static void main(String argv []){
+      System.out.print("Belajar Java ");
+	  System.out.println("Belajar Java ");}
+	}
---
+++ Minggu ke-2/belajar2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class belajar2 {
+  public static void main(String argv []){
+	Scanner input = new Scanner (System.in);
+	String nama;
+	nama = input.nextLine();
+  }
+}
