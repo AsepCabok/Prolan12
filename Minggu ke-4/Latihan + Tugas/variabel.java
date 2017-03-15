d82f4def18bebd84df16191ce58f1f25bd412578
Asep Santana <santanaasep948@yahoo.co.id>
2017-03-15 03:17:53 -0800

    Add files via upload

---
+++ Minggu ke-4/Latihan + Tugas/variabel.java
@@ -0,0 +1,36 @@
+public class variabel {
+	
+	private int a;
+	private boolean menikah;
+	private char _nama;
+	public static final String JUDUL = "Belajar Java Yuks";
+	public static String judul2 = "Belajar Java Yuks";
+
+public static void main(String[] args) {
+		variabel var = new variabel();
+		final int a = 12;
+		
+		var.a =1;
+		//a=11; -> tidak bisa dilakukan
+		double aa, b, c;
+		String nama ="nama saya";
+		byte bb =1, B =2;
+		System.out.println(JUDUL);
+		System.out.println(judul2);
+		
+	}
+	
+public void tesvariabel() {
+		System.out.println(a);
+		System.out.println(menikah);
+		System.out.println(JUDUL);
+		System.out.println(judul2);
+	}
+}
+
+class Aksesvariabelstatic {
+		public void aksesvariabelStatic() {
+		System.out.println(variabel.JUDUL);
+		//variabel.JUDUL = "bELAJAR HAL LAIN" -> tidak bisa dilakukan 
+	}
+}
